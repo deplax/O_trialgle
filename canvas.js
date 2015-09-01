2bbf13685550c6cd77dd2cb5fa524c5f66051300
deplax <sturdyegg@gmail.com>
2015-09-01 21:46:31 +0900

    Javascript file upload test

--- canvas.js
+++ canvas.js
@@ -1,5 +1,6 @@
 // ctx.fillRect(0,0,150,75);
 
+//딱히 필요 없게 되버렸다.----------------------------
 function Triangle(p1, p2, p3) {
     this.p1 = p1;
     this.p2 = p2;
@@ -10,17 +11,18 @@ function Point(x, y) {
     this.x = x;
     this.y = y;
 }
+//----------------------------------------------
 
-function canvasDrawer(canvasName) {
-	this.canvas;
-	this.ctx;
+function CanvasDrawer(canvasName) {
+    this.canvas;
+    this.ctx;
 
     this.triangles = [];
 
     this.init(canvasName);
 }
 
-canvasDrawer.prototype.init = function(canvasName) {
+CanvasDrawer.prototype.init = function(canvasName) {
     this.canvas = document.getElementById(canvasName);
     this.ctx = this.canvas.getContext("2d");
 
@@ -41,21 +43,60 @@ canvasDrawer.prototype.init = function(canvasName) {
     this.ctx.imageSmoothingEnabled = false;
 }
 
-canvasDrawer.prototype.drawTriangle = function(triangle){
+CanvasDrawer.prototype.drawTriangle = function(triangle) {
 
 }
 
+CanvasDrawer.prototype.drawTriangles = function() {
+    this.triangles;
+}
 
-canvasDrawer.prototype.createTriangle = function(x1, y1, x2, y2, x3, y3){
-	var p1 = new Point(x1, y1);
-	var p2 = new Point(x2, y2);
-	var p3 = new Point(x3, y3);
-	var triangle = new Triangle(p1, p2 ,p3);
-	this.triangles.push(triangle);
+CanvasDrawer.prototype.createTriangle = function(x1, y1, x2, y2, x3, y3) {
+    var p1 = new Point(x1, y1);
+    var p2 = new Point(x2, y2);
+    var p3 = new Point(x3, y3);
+    var triangle = new Triangle(p1, p2, p3);
+    this.triangles.push(triangle);
 }
 
 //========================================================
+function ImageResizer() {
+    this.maxWidth;
+    this.maxHeight;
+    this.reader
 
-canvas = new canvasDrawer("myCanvas");
+}
+//========================================================
+canvas = new CanvasDrawer("myCanvas");
 
 p1 = new Point(10, 20);
+
+
+$("#fileUpload").change(function() {
+    file = this.files[0];
+    var reader = new FileReader();
+    console.log(1);
+    // Closure to capture the file information.
+    reader.onload = function(e) {
+        debugger;
+        e.target.result;
+    }
+});
+
+//========================================================
+
+function previewFile() { 
+    var preview = document.querySelector('img');
+    var file = document.querySelector('input[type=file]').files[0];
+    reader = new FileReader();
+
+    reader.onloadend = function() {
+        preview.src = reader.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    } else {
+        preview.src = "";
+    }
+}
